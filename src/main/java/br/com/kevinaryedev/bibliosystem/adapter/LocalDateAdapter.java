00f62f8266be8dab4ac9ed1f1fc1fdb3f72ab570
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-25 02:33:20 -0300

    feat: Criação dos Utils

--- src/main/java/br/com/kevinaryedev/bibliosystem/adapter/LocalDateAdapter.java
+++ src/main/java/br/com/kevinaryedev/bibliosystem/adapter/LocalDateAdapter.java
@@ -1,2 +1,20 @@
-package br.com.kevinaryedev.bibliosystem.adapter;public class LocalDateAdapter {
+package br.com.kevinaryedev.bibliosystem.adapter;
+
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class LocalDateAdapter extends TypeAdapter<LocalDate> {
+    @Override
+    public void write(final JsonWriter jsonWriter, final LocalDate localDate ) throws IOException {
+        jsonWriter.value(localDate.toString());
+    }
+
+    @Override
+    public LocalDate read( final JsonReader jsonReader ) throws IOException {
+        return LocalDate.parse(jsonReader.nextString());
+    }
 }
