cd5f4a6cde23759621b8f63c78e9687f5443efe6
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-25 02:37:25 -0300

    feat: Base Test

--- src/test/java/br/com/kevinaryeldev/bibliosystem/BaseTest.java
+++ src/test/java/br/com/kevinaryeldev/bibliosystem/BaseTest.java
@@ -1,2 +1,15 @@
-package br.com.kevinaryeldev.bibliosystem;public class BaseTest {
+package br.com.kevinaryeldev.bibliosystem;
+
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+    @BeforeAll
+    public static void setup(){
+        System.out.println("Iniciando os testes");
+        RestAssured.defaultParser = Parser.JSON;
+        RestAssured.baseURI = "http://localhost:8080/bibliosystem/";
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
 }
