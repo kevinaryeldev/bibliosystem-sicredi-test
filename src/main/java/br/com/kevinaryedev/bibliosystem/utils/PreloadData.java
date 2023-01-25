6caa7997f22de1bf6a73f75f2a1b9578c28f0373
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-25 02:38:15 -0300

    feat: Facade para precarregar dados

--- src/main/java/br/com/kevinaryedev/bibliosystem/utils/PreloadData.java
+++ src/main/java/br/com/kevinaryedev/bibliosystem/utils/PreloadData.java
@@ -1,2 +1,19 @@
-package br.com.kevinaryedev.bibliosystem.utils;public class PreloadData {
+package br.com.kevinaryedev.bibliosystem.utils;
+
+import br.com.kevinaryedev.bibliosystem.client.ClientClient;
+import br.com.kevinaryedev.bibliosystem.data.factory.ClientCreateRequestFactory;
+import br.com.kevinaryedev.bibliosystem.data.provider.DataProvider;
+import br.com.kevinaryedev.bibliosystem.model.request.ClientCreateRequest;
+import br.com.kevinaryedev.bibliosystem.model.response.ClientResponse;
+import io.restassured.response.Response;
+
+public class PreloadData {
+    ClientCreateRequestFactory clientCreateRequestFactory = new ClientCreateRequestFactory(DataProvider.getValidClientCreateRequest());
+
+    public ClientResponse client() {
+        ClientCreateRequest client = clientCreateRequestFactory.createClientCreateRequestValid();
+        String json = Utils.converterParaJson(client);
+        Response response = ClientClient.createClient(json);
+        return response.as(ClientResponse.class);
+    }
 }
