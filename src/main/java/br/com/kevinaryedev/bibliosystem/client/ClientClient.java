bbe3c949f61b4a03a0c2d5472ddadaa350226a86
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-25 02:33:54 -0300

    feat: Finalização da facade Client

--- src/main/java/br/com/kevinaryedev/bibliosystem/client/ClientClient.java
+++ src/main/java/br/com/kevinaryedev/bibliosystem/client/ClientClient.java
@@ -1,4 +1,4 @@
-package br.com.kevinaryedev.bibliosystem.service;
+package br.com.kevinaryedev.bibliosystem.client;
 
 import br.com.kevinaryedev.bibliosystem.data.changeless.ClientData;
 import br.com.kevinaryedev.bibliosystem.spec.RequestSpec;
@@ -6,14 +6,55 @@ import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 
-public class ClientService {
+public class ClientClient {
     public static Response createClient(String json){
         return given()
                 .log().all()
                 .spec(RequestSpec.noAuth())
                 .body(json)
             .when()
-                .post(ClientData.SERVICE_CREATE)
+                .post(ClientData.SERVICE_CREATE);
+    }
+    public static Response listClient(Integer page, Integer size){
+        return given()
+                .log().all()
+                .spec(RequestSpec.noAuthWithQueryParamPagination(page,size))
+            .when()
+                .get(ClientData.SERVICE_FIND_ALL)
+            .then()
+                .log().all()
+                .extract().response()
+        ;
+    }
+    public static Response editClient(String id, String json){
+        return given()
+                .log().all()
+                .spec(RequestSpec.noAuthWithPathParamId(id))
+                .body(json)
+            .when()
+                .put(ClientData.SERVICE_UPDATE)
+            .then()
+                .log().all()
+                .extract().response()
+        ;
+    }
+    public static Response deleteClient(String id){
+        return given()
+                .log().all()
+                .spec(RequestSpec.noAuthWithPathParamId(id))
+            .when()
+                .delete(ClientData.SERVICE_DELETE)
+            .then()
+                .log().all()
+                .extract().response()
+        ;
+    }
+    public static Response findClientById(String id){
+        return given()
+                .log().all()
+                .spec(RequestSpec.noAuthWithPathParamId(id))
+            .when()
+                .get(ClientData.SERVICE_FIND_BY_ID)
             .then()
                 .log().all()
                 .extract().response()
