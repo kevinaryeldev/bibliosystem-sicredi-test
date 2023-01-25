4a9843e6f1326d60fa99c2e71f0a4a6b7b485314
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-25 02:39:27 -0300

    feat: Testes de deleção

--- src/test/java/br/com/kevinaryeldev/bibliosystem/aceitacao/client/ClientDeleteTest.java
+++ src/test/java/br/com/kevinaryeldev/bibliosystem/aceitacao/client/ClientDeleteTest.java
@@ -1,2 +1,42 @@
-package br.com.kevinaryeldev.bibliosystem.aceitacao.cliente;public class ClientDeleteTest {
+package br.com.kevinaryeldev.bibliosystem.aceitacao.client;
+
+import br.com.kevinaryedev.bibliosystem.client.ClientClient;
+import br.com.kevinaryedev.bibliosystem.model.response.ClientResponse;
+import br.com.kevinaryedev.bibliosystem.utils.PreloadData;
+import io.qameta.allure.*;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.Assert;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class ClientDeleteTest {
+    PreloadData preloadData = new PreloadData();
+    @Test
+    @Tag("aceitacao")
+    @Tag("cliente")
+    @Owner("Kevin Aryel")
+    @Epic("Aceitação")
+    @Feature("Cliente")
+    @Story("Deletar cliente")
+    @Description("Deve deletar um cliente com sucesso")
+    public void deveRetornarUmClienteComSucesso() {
+        ClientResponse clientResponse = preloadData.client();
+        String id = clientResponse.getId().toString();
+        Response response = ClientClient.deleteClient(id);
+        Assert.assertEquals(HttpStatus.SC_NO_CONTENT, response.getStatusCode());
+    }
+    @Test
+    @Tag("aceitacao")
+    @Tag("cliente")
+    @Owner("Kevin Aryel")
+    @Epic("Aceitação")
+    @Feature("Cliente")
+    @Story("Deletar cliente")
+    @Description("Deve retornar erro ao tentar deletar um cliente com id inválido")
+    public void deveRetornarErroAoTentarDeletarUmClienteComIdInvalido() {
+        String id = "0";
+        Response response = ClientClient.deleteClient(id);
+        Assert.assertEquals(HttpStatus.SC_NOT_FOUND, response.getStatusCode());
+    }
 }
