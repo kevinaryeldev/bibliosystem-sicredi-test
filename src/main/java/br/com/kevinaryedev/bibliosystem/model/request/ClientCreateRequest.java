c37745ab1da908126b97e057726e6242e85b84d4
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-25 02:37:09 -0300

    feat: Implementação de Prototype

--- src/main/java/br/com/kevinaryedev/bibliosystem/model/request/ClientCreateRequest.java
+++ src/main/java/br/com/kevinaryedev/bibliosystem/model/request/ClientCreateRequest.java
@@ -9,10 +9,18 @@ import java.time.LocalDate;
 @Setter
 @Getter
 @AllArgsConstructor
-public class ClientCreateModelRequest {
+public class ClientCreateRequest implements Cloneable{
     private String name;
     private String document;
     private String email;
     private Character gender;
     private LocalDate birth_date;
+
+    public ClientCreateRequest clone() {
+        try {
+            return (ClientCreateRequest) super.clone();
+        }  catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
