e60802a5ecf4637ebb4e2b564882f8cabd35f9af
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-25 05:57:46 -0300

    feat: preload para exemplares

--- src/main/java/br/com/kevinaryedev/bibliosystem/utils/PreloadData.java
+++ src/main/java/br/com/kevinaryedev/bibliosystem/utils/PreloadData.java
@@ -2,17 +2,22 @@ package br.com.kevinaryedev.bibliosystem.utils;
 
 import br.com.kevinaryedev.bibliosystem.client.BookClient;
 import br.com.kevinaryedev.bibliosystem.client.ClientClient;
+import br.com.kevinaryedev.bibliosystem.client.CopyClient;
 import br.com.kevinaryedev.bibliosystem.data.factory.BookCreateRequestFactory;
 import br.com.kevinaryedev.bibliosystem.data.factory.ClientCreateRequestFactory;
+import br.com.kevinaryedev.bibliosystem.data.factory.CopyCreateRequestFactory;
 import br.com.kevinaryedev.bibliosystem.data.provider.DataProvider;
 import br.com.kevinaryedev.bibliosystem.model.request.BookCreateRequest;
 import br.com.kevinaryedev.bibliosystem.model.request.ClientCreateRequest;
+import br.com.kevinaryedev.bibliosystem.model.request.CopyCreateRequest;
 import br.com.kevinaryedev.bibliosystem.model.response.BookResponse;
 import br.com.kevinaryedev.bibliosystem.model.response.ClientResponse;
+import br.com.kevinaryedev.bibliosystem.model.response.CopyResponse;
 import io.restassured.response.Response;
 
 public class PreloadData {
 
+    CopyCreateRequestFactory copyCreateRequestFactory = CopyCreateRequestFactory.getInstance();
     ClientCreateRequestFactory clientCreateRequestFactory = ClientCreateRequestFactory.getInstance();
     BookCreateRequestFactory bookCreateRequestFactory = BookCreateRequestFactory.getInstance();
 
@@ -28,5 +33,12 @@ public class PreloadData {
         String json = Utils.converterParaJson(book);
         Response response = BookClient.createBook(json);
         return response.as(BookResponse.class);
+    }
+    public CopyResponse copy(String bookId) {
+        Integer id = Integer.parseInt(bookId);
+        CopyCreateRequest copy = copyCreateRequestFactory.createCopyCreateRequestValid(id);
+        String json = Utils.converterParaJson(copy);
+        Response response = CopyClient.createCopy(json);
+        return response.as(CopyResponse.class);
     }
 }
