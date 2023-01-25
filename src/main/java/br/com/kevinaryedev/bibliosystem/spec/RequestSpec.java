92db7f6f14588c8e9ec417fc25c80220bbfd419d
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-25 02:35:15 -0300

    feat: Facade para specs

--- src/main/java/br/com/kevinaryedev/bibliosystem/spec/RequestSpec.java
+++ src/main/java/br/com/kevinaryedev/bibliosystem/spec/RequestSpec.java
@@ -1,2 +1,27 @@
-package br.com.kevinaryedev.bibliosystem.spec;public class RequestSpec {
+package br.com.kevinaryedev.bibliosystem.spec;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+public class RequestSpec {
+    public static RequestSpecification noAuth() {
+        return new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+    public static RequestSpecification noAuthWithPathParamId(String id){
+        return new RequestSpecBuilder()
+                .addPathParam("id",id)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+    public static RequestSpecification noAuthWithQueryParamPagination(Integer page,Integer size){
+        return new RequestSpecBuilder()
+                .addQueryParam("page",page)
+                .addQueryParam("size",size)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
 }
+
